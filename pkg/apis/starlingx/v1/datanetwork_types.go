b4d25ea032638314aa515e56ae58cf1d4f676f22
Don Penney <don.penney@windriver.com>
2020-02-24 13:29:17 -0500

    Add reconciled state to resource info
    This commit adds the reconciled state to the displayed resource info
    for a "kubectl get" for the datanetwork, host, platformnetwork, and
    system resources.
    
    Signed-off-by: Don Penney <don.penney@windriver.com>

--- pkg/apis/starlingx/v1/datanetwork_types.go
+++ pkg/apis/starlingx/v1/datanetwork_types.go
@@ -90,6 +90,7 @@ type DataNetworkStatus struct {
 // +kubebuilder:subresource:status
 // +kubebuilder:printcolumn:name="type",type="string",JSONPath=".spec.type",description="The data network encapsulation type."
 // +kubebuilder:printcolumn:name="insync",type="boolean",JSONPath=".status.inSync",description="The current synchronization state."
+// +kubebuilder:printcolumn:name="reconciled",type="boolean",JSONPath=".status.reconciled",description="The current reconciliation state."
 type DataNetwork struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
