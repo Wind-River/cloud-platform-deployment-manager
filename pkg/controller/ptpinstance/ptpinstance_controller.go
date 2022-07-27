5ca36bd1b7e26907dad96d8c65773198e4dd4166
Hugo Nicodemos <hugonicodemos@users.noreply.github.com>
2022-07-27 18:34:47 -0300

    upversion to v2.0.8-2 (#115)
    * Fix ptp controllers reconcile
    
    The ptpinstance/interface reconcile update was
    not checking if the host was already synced, this
    adds a verification before trying to sync with the
    DM configuration again.
    
    Test Plan:
    1. Host with basic ptp configuration in the DM
    2. Change ptp instance/interface parameters
    - system ptp-instance-parameter-add PTP_INSTANCE KEY=VALUE
    - system ptp-interface-paramter-add PTP_INTERFACE KEY=VALUE
    3. Check if the parameters was updated
    - /etc/ptpinstance/
    - system ptp-instance-show PTP_INSTANCE
    - system ptp-interface-show PTP_INTERFACE
    4. lock/unlock the host
    5. Check parameters still updated
    
    Signed-off-by: Hugo Brito <hugo.brito@windriver.com>
    
    * Upversion to 2.0.8-2
    
    Signed-off-by: Hugo Brito <hugo.brito@windriver.com>

--- pkg/controller/ptpinstance/ptpinstance_controller.go
+++ pkg/controller/ptpinstance/ptpinstance_controller.go
@@ -306,6 +306,18 @@ func (r *ReconcilePtpInstance) FindExistingPTPInstance(client *gophercloud.Servi
 // match the desired state of the resource.
 func (r *ReconcilePtpInstance) ReconcileUpdated(client *gophercloud.ServiceClient, instance *starlingxv1.PtpInstance, existing *ptpinstances.PTPInstance) error {
 	if ok := instanceUpdateRequired(instance, existing); ok {
+		if instance.Status.Reconciled && r.StopAfterInSync() {
+			// Do not process any further changes once we have reached a
+			// synchronized state unless there is an annotation on the resource.
+			if _, present := instance.Annotations[cloudManager.ReconcileAfterInSync]; !present {
+				msg := common.NoProvisioningAfterReconciled
+				r.ReconcilerEventLogger.NormalEvent(instance, common.ResourceUpdated, msg)
+				return common.NewChangeAfterInSync(msg)
+			} else {
+				log.Info(common.ProvisioningAllowedAfterReconciled)
+			}
+		}
+
 		// As there's not sysinv API to update the name and service type of a
 		// PTP instance, delete the existing and create a new one.
 		log.Info("deleting PTP instance", "status", instance.Status)
@@ -327,6 +339,18 @@ func (r *ReconcilePtpInstance) ReconcileUpdated(client *gophercloud.ServiceClien
 			"ptp instance has been updated")
 
 	} else if added, removed, required := instanceParameterUpdateRequired(instance, existing); required {
+		if instance.Status.Reconciled && r.StopAfterInSync() {
+			// Do not process any further changes once we have reached a
+			// synchronized state unless there is an annotation on the resource.
+			if _, present := instance.Annotations[cloudManager.ReconcileAfterInSync]; !present {
+				msg := common.NoProvisioningAfterReconciled
+				r.ReconcilerEventLogger.NormalEvent(instance, common.ResourceUpdated, msg)
+				return common.NewChangeAfterInSync(msg)
+			} else {
+				log.Info(common.ProvisioningAllowedAfterReconciled)
+			}
+		}
+
 		// Update PTP parameters associated with PTP instance
 		if len(added) > 0 {
 			new, err := r.ReconcileParamAdded(client, added, existing)
