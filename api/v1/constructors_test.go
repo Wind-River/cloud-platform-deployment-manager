Wind-River/cloud-platform-deployment-manager

commit b41223ff73d94bbe8c2c068fdbe942a11bb2b457

Fix device path if it is offered as device node (#170)
This commit fix the issue if the device path is offered as
device node in the deployment configurations.

Test plan
1 . Deploy a storage system with the root_device, boot_device
are configured as /dev/sda, the cgts-lv is configured as /dev/sda5,
the osd is configured as /dev/sdb in the controller-0
2. Varify the system is deployed with the device path associated
with the device node. All resource are in-sync and reconciled after
the deployment.

Signed-off-by: Yuxing Jiang <Yuxing.Jiang@windriver.com>

Signed-off-by: Yuxing Jiang <Yuxing.Jiang@windriver.com>

api/v1/constructors.go
@@ -1,5 +1,5 @@
 /* SPDX-License-Identifier: Apache-2.0 */
-/* Copyright(c) 2019-2022 Wind River Systems, Inc. */
+/* Copyright(c) 2019-2023 Wind River Systems, Inc. */
 
 package v1
 
@@ -525,7 +525,7 @@ func parsePhysicalVolumeInfo(group *VolumeGroupInfo, vg *volumegroups.VolumeGrou
 // parsePartitionInfo is a utility which parses the partition data as it is
 // presented by the system API and stores the data in the form required by a
 // profile spec.
-func parseVolumeGroupInfo(profile *HostProfileSpec, host v1info.HostInfo) error {
+func ParseVolumeGroupInfo(profile *HostProfileSpec, host v1info.HostInfo) error {
 	result := make([]VolumeGroupInfo, len(host.VolumeGroups))
 
 	for i, vg := range host.VolumeGroups {
@@ -662,7 +662,7 @@ func parseStorageInfo(profile *HostProfileSpec, host v1info.HostInfo) error {
 	}
 
 	// Fill-in partition attributes
-	err = parseVolumeGroupInfo(profile, host)
+	err = ParseVolumeGroupInfo(profile, host)
 	if err != nil {
 		return err
 	}
@@ -744,11 +744,11 @@ func NewNamespace(name string) (*v1.Namespace, error) {
 	return &namespace, nil
 }
 
-// fixDevicePath is a utility function that take a legacy formatted device
+// FixDevicePath is a utility function that take a legacy formatted device
 // path (e.g., sda or /dev/sda) and convert it to the newer format which is
 // more explicit
 // (e.g., /dev/disk/by-path/pci-0000:00:14.0-usb-0:1:1.0-scsi-0:0:0:0).
-func fixDevicePath(path string, host v1info.HostInfo) string {
+func FixDevicePath(path string, host v1info.HostInfo) string {
 	shortFormNode := regexp.MustCompile(`(?s)^\w+$`)
 	longFormNode := regexp.MustCompile(`(?s)^/dev/\w+$`)
 
@@ -808,9 +808,9 @@ func NewHostProfileSpec(host v1info.HostInfo) (*HostProfileSpec, error) {
 		spec.Location = host.Location.Name
 	}
 
-	bootDevice := fixDevicePath(host.BootDevice, host)
+	bootDevice := FixDevicePath(host.BootDevice, host)
 	spec.BootDevice = &bootDevice
-	rootDevice := fixDevicePath(host.RootDevice, host)
+	rootDevice := FixDevicePath(host.RootDevice, host)
 	spec.RootDevice = &rootDevice
 	clock := *host.ClockSynchronization
 	clockCopied := clock[0:] // Copy ClockSynchronization value
@@ -873,7 +873,7 @@ func NewHostProfileSpec(host v1info.HostInfo) (*HostProfileSpec, error) {
 		return nil, err
 	}
 
-	// Fill-in Route attributes
+	// Fill-in Storage attributes
 	err = parseStorageInfo(&spec, host)
 	if err != nil {
 		return nil, err

api/v1/constructors_test.go
@@ -1,5 +1,5 @@
 /* SPDX-License-Identifier: Apache-2.0 */
-/* Copyright(c) 2019-2022 Wind River Systems, Inc. */
+/* Copyright(c) 2019-2023 Wind River Systems, Inc. */
 
 package v1
 
@@ -88,7 +88,7 @@ var _ = Describe("Constructor utils for kind", func() {
 						want: "/dev/sdc"},
 				}
 				for _, tt := range tests {
-					got := fixDevicePath(tt.args.path, tt.args.host)
+					got := FixDevicePath(tt.args.path, tt.args.host)
 					Expect(reflect.DeepEqual(got, tt.want)).To(BeTrue())
 				}
 			})

controllers/host/host_controller.go
@@ -1,5 +1,5 @@
 /* SPDX-License-Identifier: Apache-2.0 */
-/* Copyright(c) 2019-2022 Wind River Systems, Inc. */
+/* Copyright(c) 2019-2023 Wind River Systems, Inc. */
 
 package host
 
@@ -1318,8 +1318,6 @@ func (r *HostReconciler) ReconcileExistingHost(client *gophercloud.ServiceClient
 	}
 	defaults.BoardManagement = &bmInfo
 
-	FixProfileAttributes(defaults, profile, current)
-
 	// Create a new composite profile that is backed by the host's default
 	// configuration.  This will ensure that if a user deletes an optional
 	// attribute that we will know how to restore the original value.
@@ -1327,6 +1325,11 @@ func (r *HostReconciler) ReconcileExistingHost(client *gophercloud.ServiceClient
 	if err != nil {
 		return err
 	}
+
+	// Fix attributes in profiles to a uniformed format
+	// As the Merge Profiles will overwrite some formate in the default profile
+	// parsed in the constructor, move this process after it.
+	FixProfileAttributes(defaults, profile, current, &hostInfo)
 
 	// TODO(alegacy): Need to move ProvisioningMode out of the profile or
 	//  find a way to populate it into profiles generated from the running

controllers/host/profile_utils.go
@@ -1,5 +1,5 @@
 /* SPDX-License-Identifier: Apache-2.0 */
-/* Copyright(c) 2019-2022 Wind River Systems, Inc. */
+/* Copyright(c) 2019-2023 Wind River Systems, Inc. */
 
 package host
 
@@ -16,6 +16,7 @@ import (
 	perrors "github.com/pkg/errors"
 	starlingxv1 "github.com/wind-river/cloud-platform-deployment-manager/api/v1"
 	"github.com/wind-river/cloud-platform-deployment-manager/controllers/common"
+	v1info "github.com/wind-river/cloud-platform-deployment-manager/platform"
 	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/types"
 )
@@ -33,7 +34,7 @@ func MergeProfiles(a, b *starlingxv1.HostProfileSpec) (*starlingxv1.HostProfileS
 }
 
 // FixProfileAttributes makes some adjustments to profile attributes
-func FixProfileAttributes(a, b, c *starlingxv1.HostProfileSpec) {
+func FixProfileAttributes(a, b, c *starlingxv1.HostProfileSpec, hostInfo *v1info.HostInfo) {
 	// To compare the BootMAC's we need to lowercase the values
 	if a.BootMAC != nil {
 		lowerDefaultsBootMAC := strings.ToLower(*a.BootMAC)
@@ -59,6 +60,43 @@ func FixProfileAttributes(a, b, c *starlingxv1.HostProfileSpec) {
 			}
 		}
 	}
+	FixProfileDevicePath(a, hostInfo)
+}
+
+// FixProfileDevicePath is to fix the device path if it is offered as device node
+func FixProfileDevicePath(a *starlingxv1.HostProfileSpec, hostInfo *v1info.HostInfo) {
+	if a.RootDevice != nil {
+		rootDevice := starlingxv1.FixDevicePath(*a.RootDevice, *hostInfo)
+		a.RootDevice = &rootDevice
+	}
+
+	if a.BootDevice != nil {
+		bootDevice := starlingxv1.FixDevicePath(*a.BootDevice, *hostInfo)
+		a.BootDevice = &bootDevice
+	}
+
+	if a.Storage.OSDs != nil {
+		FixOSDDevicePath(a, hostInfo)
+	}
+
+	if a.Storage.VolumeGroups != nil {
+		err := starlingxv1.ParseVolumeGroupInfo(a, *hostInfo)
+		if err != nil {
+			logHost.Error(err, "Failed to parse volume group")
+		}
+	}
+}
+
+// FixOSDDevicePath is to fix device path in the profile's OSD spec
+func FixOSDDevicePath(a *starlingxv1.HostProfileSpec, hostInfo *v1info.HostInfo) {
+
+	result := make([]starlingxv1.OSDInfo, 0)
+	for _, o := range *a.Storage.OSDs {
+		o.Path = starlingxv1.FixDevicePath(o.Path, *hostInfo)
+		result = append(result, o)
+	}
+	list := starlingxv1.OSDList(result)
+	a.Storage.OSDs = &list
 }
 
 // GetHostProfile retrieves a HostProfileSpec from the kubernetes API

controllers/host/storage.go
@@ -1,5 +1,5 @@
 /* SPDX-License-Identifier: Apache-2.0 */
-/* Copyright(c) 2019 Wind River Systems, Inc. */
+/* Copyright(c) 2019-2023 Wind River Systems, Inc. */
 
 package host
 

commit 4c01f8e257ea373bf0be8bfa1606e16cb0b20ac1

Cherry-pick device_path logice to stable/v2.0.10 (#191)
* Bug fix: fix volume group in default spec (#172)

The previous commit: b41223ff73d94bbe8c2c068fdbe942a11bb2b457(PR: #170)
introduced a bug when fixing the device path of the physical volumes
in volume groups. And it escaped the test in that commit without an
extra volume configured.

This commit re-write the function to fix the device path of the physical
volumes rather than re-using the exising parse fucntion in the
constructor to address this issue.

Test plan:
Passed: deploy an AIOSX system with cgts-lv configured in sda and nova-
local volume configured in sdc.

Signed-off-by: Yuxing Jiang <Yuxing.Jiang@windriver.com>
(cherry picked from commit 568f5a3f6c77af11e84d2188011e66532ef4985d)

* Add support for device node mapper format (#188)

This commit changes the method to identify the device path and
device node. It returns the device path starts from /dev/disk/,
then tries to use the string as device node to find the device
path, finally returns the string as device path if finding nothing
matches.

With this commit, the following format will be accepted:
1 Short format: e.g. sda
2 Long format: e.g. /dev/sda, /dev/sdaa, /dev/mapper/mpatha
3 Path format: e.g.
  /dev/disk/by-path/pci-0000:00:14.0-usb-0:1:1.0-scsi-0:0:0:0,
  /dev/disk/by-id/pci-0000:00:14.0-usb-0:1:1.0-scsi-0:0:0:0

Not supported format:
1 Incompelete long format of device node: e.g.
  mpatha
2 Incompelete shor format of device path: e.g.
  pci-0000:00:14.0-usb-0:1:1.0-scsi-0:0:0:0

Test plan:
1 Deploy use device node(long, short) - pass
2 Deploy use device path - pass

Signed-off-by: Yuxing Jiang <Yuxing.Jiang@windriver.com>
(cherry picked from commit 9d2d155491f35066e38a472179110155ea0c3e73)

api/v1/constructors.go
@@ -749,17 +749,21 @@ func NewNamespace(name string) (*v1.Namespace, error) {
 // more explicit
 // (e.g., /dev/disk/by-path/pci-0000:00:14.0-usb-0:1:1.0-scsi-0:0:0:0).
 func FixDevicePath(path string, host v1info.HostInfo) string {
+	// device path starts from /dev/disk/*
+	formPath := regexp.MustCompile(`(?s)^/dev/disk/.*`)
+	// e.g. sda
 	shortFormNode := regexp.MustCompile(`(?s)^\w+$`)
-	longFormNode := regexp.MustCompile(`(?s)^/dev/\w+$`)
 
 	var searchPath string
-	if shortFormNode.MatchString(path) {
-		searchPath = fmt.Sprintf("/dev/%s", path)
-	} else if longFormNode.MatchString(path) {
-		searchPath = path
-	} else {
-		// Likely already in the devicePath format
+	if formPath.MatchString(path) {
+		// Find the device path
 		return path
+	} else if shortFormNode.MatchString(path) {
+		// Append /dev/ to the short format to get full format of device node
+		searchPath = fmt.Sprintf("/dev/%s", path)
+	} else {
+		// For the rest formats, likely is a full format of a device node
+		searchPath = path
 	}
 
 	if disk, ok := host.FindDiskByNode(searchPath); ok {

api/v1/constructors_test.go
@@ -44,7 +44,7 @@ var _ = Describe("Constructor utils for kind", func() {
 		})
 	})
 
-	Describe("fixDevicePath utility", func() {
+	Describe("FixDevicePath utility", func() {
 		Context("with path data", func() {
 			It("should convert it to the newer format", func() {
 				type args struct {
@@ -61,6 +61,8 @@ var _ = Describe("Constructor utils for kind", func() {
 							DevicePath: "/dev/disk/by-path/pci-0000:00:16.0-usb-0:1:1.0-scsi-0:0:0:0"},
 						{DeviceNode: "/dev/sdb",
 							DevicePath: "/dev/disk/by-path/pci-0000:00:17.0-usb-0:1:1.0-scsi-0:0:0:0"},
+						{DeviceNode: "/dev/mapper/mpatha",
+							DevicePath: "/dev/disk/by-id/pci-0000:00:18.0-usb-0:1:1.0-scsi-0:0:0:0"},
 					},
 				}
 				tests := []struct {
@@ -86,6 +88,18 @@ var _ = Describe("Constructor utils for kind", func() {
 					{name: "not-found",
 						args: args{path: "/dev/sdc", host: host1},
 						want: "/dev/sdc"},
+					{name: "imcomplete-mapper-not-found",
+						args: args{path: "mpatha", host: host1},
+						want: "mpatha"},
+					{name: "mapper-form",
+						args: args{path: "/dev/mapper/mpatha", host: host1},
+						want: "/dev/disk/by-id/pci-0000:00:18.0-usb-0:1:1.0-scsi-0:0:0:0"},
+					{name: "path-form",
+						args: args{path: "/dev/disk/by-id/pci-0000:00:18.0-usb-0:1:1.0-scsi-0:0:0:0", host: host1},
+						want: "/dev/disk/by-id/pci-0000:00:18.0-usb-0:1:1.0-scsi-0:0:0:0"},
+					{name: "imcomplete-path-form-not-found",
+						args: args{path: "pci-0000:00:18.0-usb-0:1:1.0-scsi-0:0:0:0", host: host1},
+						want: "pci-0000:00:18.0-usb-0:1:1.0-scsi-0:0:0:0"},
 				}
 				for _, tt := range tests {
 					got := FixDevicePath(tt.args.path, tt.args.host)