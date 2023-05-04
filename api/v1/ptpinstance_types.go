4c924e29e3ab8c178995e0a6a67e7ca961785f37
Enzo Candotti <enzo.candotti@windriver.com>
2023-05-04 15:40:16 -0300

    Fix position of RECONCILE column in resource list
    This commits switches the positions of DeploymentScope and Reconciled
    columns in order to align with the Ansible playbook block that is
    getting the Reconciled value.
    
    In a future it would be better to rework this playbook to locate the column
    number instead of assuming that the Reconciled value is always the last
    one.
    
    Test Plan:
    PASS: Build helm chart and install DM. Verify the columns are switched.
    
    Signed-off-by: Enzo Candotti <enzo.candotti@windriver.com>

--- api/v1/ptpinstance_types.go
+++ api/v1/ptpinstance_types.go
@@ -45,8 +45,9 @@ type PtpInstanceStatus struct {
 // +deepequal-gen=false
 // +kubebuilder:subresource:status
 // +kubebuilder:printcolumn:name="insync",type="boolean",JSONPath=".status.inSync",description="The current synchronization state."
-// +kubebuilder:printcolumn:name="reconciled",type="boolean",JSONPath=".status.reconciled",description="The current reconciliation state."
 // +kubebuilder:printcolumn:name="scope",type="string",JSONPath=".status.deploymentScope",description="The current deploymentScope state."
+// +kubebuilder:printcolumn:name="reconciled",type="boolean",JSONPath=".status.reconciled",description="The current reconciliation state."
+// +TODO(ecandotti): enhance docs/playbooks/wind-river-cloud-platform-deployment-manager.yaml#L431 since it's looking for the last column to get 'reconciled' value.
 // PtpInstance is the Schema for the ptpinstances API
 type PtpInstance struct {
 	metav1.TypeMeta   `json:",inline"`
--- api/v1/ptpinterface_types.go
+++ api/v1/ptpinterface_types.go
@@ -46,8 +46,9 @@ type PtpInterfaceStatus struct {
 // +deepequal-gen=false
 // +kubebuilder:subresource:status
 // +kubebuilder:printcolumn:name="insync",type="boolean",JSONPath=".status.inSync",description="The current synchronization state."
-// +kubebuilder:printcolumn:name="reconciled",type="boolean",JSONPath=".status.reconciled",description="The current reconciliation state."
 // +kubebuilder:printcolumn:name="scope",type="string",JSONPath=".status.deploymentScope",description="The current deploymentScope state."
+// +kubebuilder:printcolumn:name="reconciled",type="boolean",JSONPath=".status.reconciled",description="The current reconciliation state."
+// +TODO(ecandotti): enhance docs/playbooks/wind-river-cloud-platform-deployment-manager.yaml#L431 since it's looking for the last column to get 'reconciled' value.
 type PtpInterface struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
