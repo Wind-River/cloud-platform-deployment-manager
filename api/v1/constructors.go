40fd4016ef311c1ed81f8aebfad393dd0d6f0cf9
vineela-pachchipulusu <Vineela.Pachchipulusu@windriver.com>
2023-09-01 00:59:43 -0400

    removes few attributes from deploy config file

--- api/v1/constructors.go
+++ api/v1/constructors.go
@@ -935,7 +935,7 @@ func parseCertificateInfo(spec *SystemSpec, certificates []certificates.Certific
 
 	for index, c := range certificates {
 		// Ignore OpenLDAP certificate since it's being installed during the initial unlock.
-		if c.Type == "openldap" {
+		if c.Type == "openldap" || c.Type == "ssl_ca" {
 			continue
 		}
 		cert := CertificateInfo{
@@ -981,12 +981,14 @@ func parseFileSystemInfo(spec *SystemSpec, fileSystems []controllerFilesystems.F
 	result := make([]ControllerFileSystemInfo, 0)
 
 	for _, fs := range fileSystems {
-		info := ControllerFileSystemInfo{
-			Name: fs.Name,
-			Size: fs.Size,
-		}
+		if fs.Name == "backup" || fs.Name == "database" {
+			info := ControllerFileSystemInfo{
+				Name: fs.Name,
+				Size: fs.Size,
+			}
 
-		result = append(result, info)
+			result = append(result, info)
+		}
 	}
 
 	if spec.Storage == nil {
@@ -1074,15 +1076,6 @@ func NewSystemSpec(systemInfo v1info.SystemInfo) (*SystemSpec, error) {
 	}
 
 	spec.VSwitchType = &systemInfo.Capabilities.VSwitchType
-
-	if systemInfo.DRBD != nil {
-		spec.Storage = &SystemStorageInfo{
-			DRBD: &DRBDConfiguration{
-				LinkUtilization: systemInfo.DRBD.LinkUtilization,
-			},
-		}
-	}
-
 	if systemInfo.DNS != nil {
 		if systemInfo.DNS.Nameservers != "" {
 			nameservers := StringsToDNSServerList(strings.Split(systemInfo.DNS.Nameservers, ","))
