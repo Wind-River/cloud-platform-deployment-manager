e1d7451fcd53b8a75857941c77122c7522dc9274
Enzo Candotti <enzo.candotti@windriver.com>
2023-10-30 18:51:36 -0300

    Change logLevel for Host delta configuration
    It was seen that the 'controller.host.deployment/controller-0 delta
    configuration' log was flooding the DM logs.
    
    This change updates the logLevel for this particular log to level 2.
    
    Test Plan:
    PASS: Verify that the log is not being shown when the logLevel is not
    modified.
    PASS: Re-install DM changing the logLevel to 2. Verify that the Host
    delta is being shown.
    
    Signed-off-by: Enzo Candotti <enzo.candotti@windriver.com>

--- controllers/host/host_controller.go
+++ controllers/host/host_controller.go
@@ -1453,7 +1453,7 @@ func (r *HostReconciler) ReconcileExistingHost(client *gophercloud.ServiceClient
 	}
 
 	if deltaString != "" {
-		logHost.Info(fmt.Sprintf("delta configuration:%s\n", deltaString))
+		logHost.V(2).Info(fmt.Sprintf("delta configuration:%s\n", deltaString))
 		instance.Status.Delta = deltaString
 
 		err = r.Client.Status().Update(context.TODO(), instance)
