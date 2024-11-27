7a2518e6b2f402f3a36d9762248e3b788804dc4a
Wallysson <wallysson.silva@windriver.com>
2024-11-27 17:06:51 -0300

    Increase Gomega Eventually timeout to 30 seconds (#430)
    This commit increases the Gomago Eventuall timeout to 30 seconds in
    all tests cases to avoid tests failures by timeout [1].
    
    [1]: https://github.com/Wind-River/cloud-platform-deployment-manager/pull/429/checks?check_run_id=33619644827
    
    Test Case:
    PASS - make test
    
    Signed-off-by: Wallysson Silva <wallysson.silva@windriver.com>
    (cherry picked from commit 675c541c3fc5bf6f6d7b4452760190f74fd401f1)

--- controllers/platformnetwork_controller_test.go
+++ controllers/platformnetwork_controller_test.go
@@ -16,7 +16,7 @@ import (
 
 var _ = Describe("Platformnetwork controller", func() {
 	const (
-		timeout  = time.Second * 20
+		timeout  = time.Second * 30
 		interval = time.Millisecond * 250
 	)
 
