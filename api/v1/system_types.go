99ce11b5474b6b508779f40c9413574cac890284
Yuxing Jiang <Yuxing.Jiang@windriver.com>
2025-01-24 10:46:25 -0500

    Revert certificate CRD to allow historical cert types
    During the upgrade from a previous release to the current release to the 24.09 release,
    the migration process is using B&R the custom resource rather than using helm to controll
    them. The legacy cert type needs to be preserved during the apply in order to accept the
    historical data with additional cert type.
    
    This commit add the legacy cert types back in helm chart to unblock the addmission, however
    the legacy certs are still expected to be deleted by clean_deprecated_certificates.
    
    Test:
    1. Build an image and fresh deploy 24.09.1 system
    2. Upgrade from 22.12 to 24.09 w/o failure
    
    Signed-off-by: Yuxing Jiang <Yuxing.Jiang@windriver.com>

--- api/v1/system_types.go
+++ api/v1/system_types.go
@@ -33,7 +33,7 @@ const (
 // is required when defining these resources.
 type CertificateInfo struct {
 	// Type represents the intended usage of the certificate
-	// +kubebuilder:validation:Enum=ssl_ca
+	// +kubebuilder:validation:Enum=ssl;ssl_ca;openstack;openstack_ca;docker_registry;tpm_mode
 	Type string `json:"type"`
 
 	// Secret is the name of a TLS secret containing the public certificate and
